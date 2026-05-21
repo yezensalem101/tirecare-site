64910143230d7126efa04426bb95ec55fd1d7048
yezensalem101 <yezensalem101@gmail.com>
2026-05-21 14:43:26 -0400

    Create layout.jsx

---
+++ app/layout.jsx
@@ -0,0 +1,12 @@
+export const metadata = {
+  title: "Tire Care Mobile Service",
+  description: "24/7 Mobile Tire Service in Tampa Bay",
+};
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="en">
+      <body>{children}</body>
+    </html>
+  );
+}
